94f741972354065ca0291e5f1196a7f2c7f76552
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-07-13 19:49:47 +0200

    add OrderedMap container

---
+++ ogsCore/BaseLib/OrderedMap.h
@@ -0,0 +1,47 @@
+
+#pragma once
+
+#include <map>
+#include <vector>
+
+namespace BaseLib
+{
+
+template <typename T_KEY, typename T_VAL>
+class OrderedMap
+{
+public:
+	typedef typename std::map<T_KEY, T_VAL>::iterator iterator;
+	typedef typename std::map<T_KEY, T_VAL>::const_iterator const_iterator;
+
+	void insert(const T_KEY &key, const T_VAL &val)
+	{
+		if (_map.count(key)==0)
+			_vec.push_back(key);
+		_map[key] = val;
+	}
+
+	size_t count(const T_KEY &key) const
+	{
+		return _map.count(key);
+	}
+
+	size_t size() const {return _vec.size();};
+
+	iterator begin() {return _map.begin();};
+	const_iterator begin() const {return _map.begin();};
+	iterator end() {return _map.end();};
+	const_iterator end() const {return _map.end();};
+
+	void clear()
+	{
+		_map.clear();
+		_vec.clear();
+	}
+
+private:
+	std::map<T_KEY, T_VAL> _map;
+	std::vector<T_KEY> _vec;
+};
+
+}
