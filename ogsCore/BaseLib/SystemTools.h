03afa0879760d5904e3334a22026a4673a8481b9
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-07-16 18:00:23 +0200

    add system util

---
+++ ogsCore/BaseLib/SystemTools.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+
+namespace BaseLib
+{
+
+inline bool IsLittleEndian()
+{
+	int x = 0x00000001;
+	if (*(char*)&x)
+		return true;              //am little
+	else
+		return false;             //am big
+}
+
+}
