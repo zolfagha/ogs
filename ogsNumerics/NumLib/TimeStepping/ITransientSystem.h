8d6787dcdb2e9dc5ef571f985cbd03b8c757bf0f
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-07-17 19:44:42 +0200

    add initialize and finalize

--- ogsNumerics/NumLib/TimeStepping/ITransientSystem.h
+++ ogsNumerics/NumLib/TimeStepping/ITransientSystem.h
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include "BaseLib/Options.h"
 #include "TimeStep.h"
 
 namespace NumLib
@@ -14,12 +15,18 @@ class ITransientSystem
 public:
 	virtual ~ITransientSystem() {};
 
+	/// initialize
+	virtual void initialize(const BaseLib::Options &op) {};
+	/// finalize
+	virtual void finalize() {};
+
     virtual double suggestNext(const TimeStep &time_current) = 0;
     virtual bool isAwake(const TimeStep &time) = 0;
     virtual int solveTimeStep(const TimeStep &time) = 0;
     const TimeStep& getCurrentTime() const {return *_current_time;};
     void setCurrentTime(const TimeStep &t) {_current_time = const_cast<TimeStep*>(&t);};
     virtual void accept(const TimeStep &time) = 0;
+
 private:
     TimeStep* _current_time;
 };
