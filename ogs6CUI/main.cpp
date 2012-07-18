c4eded94aa867a0c1aa64fc56395b669c47fba36
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-07-19 00:11:18 +0200

    add copryright

--- ogs6CUI/main.cpp
+++ ogs6CUI/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.com)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.com/LICENSE.txt
+ *
+ *
+ * \file main.cpp
+ *
+ * Created on 2012-07-17 by Norihiro Watanabe
+ */
 
 #include <iostream>
 #include <exception>
