9898ed80e97c4312f9d74b8b97bd8be4675574e5
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-07-19 20:42:43 +0200

    add a class for source term not Neumann

--- ogsNumerics/SolutionLib/FemProblem/FemNeumannBC.h
+++ ogsNumerics/SolutionLib/FemProblem/FemNeumannBC.h
@@ -111,6 +111,74 @@ private:
 
 };
 
+/**
+ * 
+ */
+class FemSourceTerm : public IFemNeumannBC 
+{
+public:
+    /// 
+    FemSourceTerm(MeshLib::IMesh *msh, const GeoLib::GeoObject *geo, NumLib::ITXFunction *func)
+    {
+        _msh = msh;
+        _geo = geo;
+        _bc_func = func->clone();
+        _is_transient = false;
+        _do_setup = true;
+    }
 
+    /// setup  
+    void setup()
+    {
+        if (!_do_setup) return;
+        if (!_is_transient) _do_setup = false;
+
+        MeshLib::findNodesOnGeometry(_msh, _geo, &_vec_nodes);
+        _vec_values.resize(_vec_nodes.size());
+        for (size_t i=0; i<_vec_nodes.size(); i++) {
+            const GeoLib::Point* x = _msh->getNodeCoordinatesRef(_vec_nodes[i]);
+            _bc_func->eval(x->getData(), _vec_values[i]);
+        }
+
+        if (!_is_transient)
+            _do_setup = false;
+    }
+
+    size_t getNumberOfConditions() const
+    {
+        return _vec_nodes.size();
+    }
+
+    size_t getConditionDoF( size_t i ) const
+    {
+        return _vec_nodes[i];
+    }
+
+    double getConditionValue( size_t i ) const
+    {
+        return _vec_values[i];
+    }
+
+
+    FemSourceTerm* clone() const
+    {
+        FemSourceTerm *f = new FemSourceTerm(_msh, _geo, _bc_func);
+        return f;
+    }
+
+    std::vector<size_t>& getListOfBCNodes() {return _vec_nodes;};
+    std::vector<double>& getListOfBCValues() {return _vec_values;};
+
+private:
+    // node id, var id, value
+    MeshLib::IMesh* _msh;
+    const GeoLib::GeoObject *_geo;
+    NumLib::ITXFunction *_bc_func;
+    std::vector<size_t> _vec_nodes;
+    std::vector<double> _vec_values;
+    bool _is_transient;
+    bool _do_setup;
+
+};
 
 }
