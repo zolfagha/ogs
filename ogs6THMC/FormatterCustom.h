c50b9c6d238ecf79be08ea4122025438cb142c23
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-07-19 20:42:03 +0200

    don't write level

--- ogs6THMC/FormatterCustom.h
+++ ogs6THMC/FormatterCustom.h
@@ -12,6 +12,6 @@ class FormatterCustom : public logog::FormatterGCC
     virtual TOPIC_FLAGS GetTopicFlags( const logog::Topic &topic )
     {
         return ( Formatter::GetTopicFlags( topic ) &
-                 ~( TOPIC_FILE_NAME_FLAG | TOPIC_LINE_NUMBER_FLAG ));
+                 ~( TOPIC_LEVEL_FLAG | TOPIC_FILE_NAME_FLAG | TOPIC_LINE_NUMBER_FLAG ));
     }
 };
