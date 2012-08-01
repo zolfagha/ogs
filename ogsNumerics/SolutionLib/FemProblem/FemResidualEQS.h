6595fd9be71be259b9490d9157bb166aca48ba2d
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-08-01 10:52:39 +0200

    set residual of Dirichlet BC nodes to zero in Newton nonlinear

--- ogsNumerics/SolutionLib/FemProblem/FemResidualEQS.h
+++ ogsNumerics/SolutionLib/FemProblem/FemResidualEQS.h
@@ -67,25 +67,6 @@ public:
         const NumLib::TimeStep &t_n1 = *this->_t_n1;
         SolutionVector *u_n = this->_u_n0;
 
-#if 0
-        // setup BC1 for solution increment
-        for (size_t i=0; i<_list_var.size(); i++) {
-            FemVariable* var = _list_var[i];
-            std::vector<size_t> var_bc_id;
-            std::vector<double> var_bc_val;
-            for (size_t j=0; j<var->getNumberOfDirichletBC(); j++) {
-                FemDirichletBC* bc1 = var->getDirichletBC(j);
-                bc1->setup();
-                std::vector<double> bc_value_for_dx(bc1->getListOfBCNodes().size(), .0);
-                for (size_t k=0; k<bc_value_for_dx.size(); k++) {
-                    size_t id = bc1->getListOfBCNodes()[k];
-                    double v =  bc1->getListOfBCValues()[k];
-                    (*u_n)[id] = v; 
-                }
-            }
-        }
-#endif
-
         //TODO temporally
         std::vector<SolutionVector*> vec_un;
         vec_un.push_back(const_cast<SolutionVector*>(u_n));
@@ -101,6 +82,22 @@ public:
         if (_st!=0)
             r += *_st;
 
+        // set residuals to zero for Dirichlet BC
+        std::vector<size_t> list_bc1_eqs_id;
+        std::vector<double> list_bc1_val;
+        for (size_t i=0; i<_list_var.size(); i++) {
+            FemVariable* var = _list_var[i];
+            std::vector<size_t> var_bc_id;
+            std::vector<double> var_bc_val;
+            for (size_t j=0; j<var->getNumberOfDirichletBC(); j++) {
+                FemDirichletBC* bc1 = var->getDirichletBC(j);
+                bc1->setup();
+                DiscreteLib::convertToEqsValues(*_dofManager, i, _msh->getID(), bc1->getListOfBCNodes(), bc1->getListOfBCValues(), list_bc1_eqs_id, list_bc1_val);
+            }
+        }
+        for (size_t k=0; k<list_bc1_eqs_id.size(); k++) {
+            r[list_bc1_eqs_id[k]] = .0; 
+        }
     }
 
 private:
