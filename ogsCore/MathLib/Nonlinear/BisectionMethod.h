3977def94faf52f478d52609c015dc01f48012fb
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-08-03 16:56:26 +0200

    add missing Copyrighh header to source files in MathLib

--- ogsCore/MathLib/Nonlinear/BisectionMethod.h
+++ ogsCore/MathLib/Nonlinear/BisectionMethod.h
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.com)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.com/LICENSE.txt
+ *
+ *
+ * \file BisectionMethod.h
+ *
+ * Created on 2012-06-25 by Norihiro Watanabe
+ */
 
 #pragma once
 
