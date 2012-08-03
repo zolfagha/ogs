1a43bf08c69516d07e257b7ac39739e8d55f476d
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-08-03 16:46:49 +0200

    add missing Copyrighh header to source files in BaseLib

--- ogsCore/BaseLib/OrderedMap.h
+++ ogsCore/BaseLib/OrderedMap.h
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.com)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.com/LICENSE.txt
+ *
+ *
+ * \file OrderedMap.h
+ *
+ * Created on 2012-07-13 by Norihiro Watanabe
+ */
 
 #pragma once
 
