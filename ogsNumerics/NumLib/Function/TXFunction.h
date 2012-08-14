1072594d4c6bb8c3bd6d7bcdb702483f3e0b0675
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-08-14 15:51:01 +0200

    add TXVectorFunctionAsColumnData class

--- ogsNumerics/NumLib/Function/TXFunction.h
+++ ogsNumerics/NumLib/Function/TXFunction.h
@@ -99,14 +99,14 @@ private:
 class TXFunctionConstant : public ITXFunction
 {
 public:
-    TXFunctionConstant(double val) : _vec(1,1) 
+    explicit TXFunctionConstant(double val) : _vec(1,1)
     {
         _vec(0,0) = val;
         ITXFunction::isConst(true);
         ITXFunction::isTemporallyConst(true);
         ITXFunction::isSpatiallyConst(true);
     };
-    TXFunctionConstant(const DataType &val) : _vec(val) {};
+    explicit TXFunctionConstant(const DataType &val) : _vec(val) {};
 
     virtual ~TXFunctionConstant() {};
 
@@ -175,6 +175,43 @@ private:
     T1 *_f1;
     T2 *_f2;
 };
+
+template <class F_VECTOR>
+class TXVectorFunctionAsColumnData : public ITXFunction
+{
+public:
+    explicit TXVectorFunctionAsColumnData(F_VECTOR* f_vec, size_t column_id)
+    : _f_vec(f_vec), _column_id(column_id)
+    {
+        ITXFunction::isConst(f_vec->isConst());
+        ITXFunction::isTemporallyConst(f_vec->isTemporallyConst());
+        ITXFunction::isSpatiallyConst(f_vec->isSpatiallyConst());
+    };
+
+    virtual ~TXVectorFunctionAsColumnData() {};
+
+    void resetVectorFunction(F_VECTOR* f_vec) {_f_vec = f_vec; };
+
+    virtual void eval(const TXPosition x, DataType &v) const
+    {
+        DataType tmp_v;
+        _f_vec->eval(x, tmp_v);
+        if (tmp_v.array().size()>0) {
+            v.resize(1,1);
+            v(0,0) = tmp_v(_column_id);
+        }
+    }
+
+    virtual TXVectorFunctionAsColumnData<F_VECTOR>* clone() const
+    {
+        return new TXVectorFunctionAsColumnData<F_VECTOR>(_f_vec, _column_id);
+    }
+
+private:
+    F_VECTOR* _f_vec;
+    size_t _column_id;
+};
+
 } //end
 
 
