67ed718911253d07d7a7d62635b9a03840b2a663
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-08-15 16:26:20 +0200

    do't find OpenMP automatically. this defines _OPENMP

--- scripts/cmake/Find.cmake
+++ scripts/cmake/Find.cmake
@@ -37,11 +37,11 @@ ENDIF() # WIN32 AND GIT_FOUND
 ######################
 
 # Clang does not have OpenMP support atm, see https://github.com/ufz/ogs/issues/8
-IF(NOT COMPILER_IS_CLANG)
-	FIND_PACKAGE(OpenMP)
-ENDIF () # !clang
-IF(OPENMP_FOUND)
-	SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-ENDIF()
+#IF(NOT COMPILER_IS_CLANG)
+#	FIND_PACKAGE(OpenMP)
+#ENDIF () # !clang
+#IF(OPENMP_FOUND)
+#	SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+#	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+#ENDIF()
 
