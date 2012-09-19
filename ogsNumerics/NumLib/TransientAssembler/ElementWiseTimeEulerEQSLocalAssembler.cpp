494dfc1260c61b7127fef533aa843584a669a9d1
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-09-19 14:27:23 +0200

    rewrite with Eigen style

--- ogsNumerics/NumLib/TransientAssembler/ElementWiseTimeEulerEQSLocalAssembler.cpp
+++ ogsNumerics/NumLib/TransientAssembler/ElementWiseTimeEulerEQSLocalAssembler.cpp
@@ -21,7 +21,7 @@ namespace NumLib
 /// @param local_u_n1    guess of current time step value
 /// @param local_u_n    previous time step value
 /// @param eqs            local algebraic equation
-void ElementWiseTimeEulerEQSLocalAssembler::assembly(const TimeStep &time, const MeshLib::IElement &e, const DiscreteLib::DofEquationIdTable &localDofManager, const LocalVector &local_u_n1, const LocalVector &local_u_n, LocalEquation &eqs)
+void ElementWiseTimeEulerEQSLocalAssembler::assembly(const TimeStep &time, const MeshLib::IElement &e, const DiscreteLib::DofEquationIdTable &/*localDofManager*/, const LocalVector &local_u_n1, const LocalVector &local_u_n, LocalEquation &eqs)
 {
     const double delta_t = time.getTimeStepSize();
     const size_t n_dof = eqs.getDimension();
@@ -37,27 +37,12 @@ void ElementWiseTimeEulerEQSLocalAssembler::assembly(const TimeStep &time, const
 
     LocalMatrix *localA = eqs.getA();
     LocalVector *localRHS = eqs.getRHSAsVec();
-    LocalMatrix TMP_M(n_dof, n_dof);
-    LocalMatrix TMP_M2(n_dof, n_dof);
 
     // A = 1/dt M + theta K
-    TMP_M = M;
-    TMP_M *= 1.0/delta_t;
-    (*localA) = TMP_M;
-    TMP_M = K;
-    TMP_M *= _theta;
-    (*localA) += TMP_M;
+    (*localA) = 1.0/delta_t * M + _theta * K;
     // RHS = (1/dt M - (1-theta) K) u0 + F
-    TMP_M = M;
-    TMP_M *= 1.0/delta_t;
-    TMP_M2 = TMP_M;
-    TMP_M = K;
-    TMP_M *= - (1.-_theta);
-    TMP_M2 += TMP_M;
-    //TMP_M2.axpy(1.0, &((LocalVector)local_u_n)[0], .0, localRHS);
-    *localRHS = TMP_M2 * local_u_n;
-    for (size_t i=0; i<n_dof; i++)
-        (*localRHS)[i] += F[i];
+    (*localRHS) = (1.0/delta_t * M - (1.-_theta) * K) * local_u_n;
+    (*localRHS) += F;
 }
 
 
