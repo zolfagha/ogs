37c8fdbc0b4068bda441bdd7f921943cb4cb1c91
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-09-24 14:41:39 +0200

    add header

--- ogsCore/MathLib/LinAlg/LinearEquation/SparseLinearEquation.cpp
+++ ogsCore/MathLib/LinAlg/LinearEquation/SparseLinearEquation.cpp
@@ -1,5 +1,16 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.com)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.com/LICENSE.txt
+ *
+ *
+ * \file SparseLinearEquation.cpp
+ *
+ * Created on 2012-06-25 by Norihiro Watanabe
+ */
 
-#include "SparseLinearEquations.h"
+#include "SparseLinearEquation.h"
 
 #include <iostream>
 #include <algorithm>
