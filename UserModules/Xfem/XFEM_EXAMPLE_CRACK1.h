0b0fd938ff4554039903e04e5265ca44840c28a9
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-09-25 16:47:02 +0200

    rename

--- UserModules/Xfem/XFEM_EXAMPLE_CRACK1.h
+++ UserModules/Xfem/XFEM_EXAMPLE_CRACK1.h
@@ -5,7 +5,7 @@
  *              http://www.opengeosys.com/LICENSE.txt
  *
  *
- * \file Displacement.h
+ * \file XFEM_EXAMPLE_CRACK1.h
  *
  * Created on 2012-09-20 by Norihiro Watanabe
  */
@@ -15,10 +15,12 @@
 #include <string>
 #include "BaseLib/CodingTools.h"
 #include "MeshLib/Core/IMesh.h"
-#include "FemLib/Function/FemIntegrationPointFunction.h"
+#include "DiscreteLib/Serial/DiscreteSystem.h"
+#include "FemLib/Function/FemNodalFunction.h"
+//#include "FemLib/Function/FemIntegrationPointFunction.h"
 #include "FemLib/Tools/LagrangeFeObjectContainer.h"
-#include "NumLib/Function/TXVectorFunctionAsColumnData.h"
-
+//#include "NumLib/Function/TXVectorFunctionAsColumnData.h"
+#include "NumLib/Function/DiscreteDataConvergenceCheck.h"
 #include "ProcessLib/AbstractTimeIndependentProcess.h"
 
 namespace xfem
@@ -27,33 +29,33 @@ namespace xfem
 /**
  *
  */
-template <class T_DISCRETE_SYSTEM, class T_LINEAR_SOLVER>
-class FunctionDisplacement
+//template <class T_DISCRETE_SYSTEM, class T_LINEAR_SOLVER>
+class FunctionXFEM_EXAMPLE_CRACK1
 : public ProcessLib::AbstractTimeIndependentProcess
 {
 public:
-    enum Out { Displacement=0 };
+    //enum Out { Displacement=0 };
 
-    typedef T_DISCRETE_SYSTEM MyDiscreteSystem;
-    typedef T_LINEAR_SOLVER MyLinearSolver;
-
-    typedef typename FemLib::FemNodalFunctionScalar<MyDiscreteSystem>::type MyNodalFunctionScalar;
+    typedef DiscreteLib::DiscreteSystem MyDiscreteSystem;
+//    //typedef T_LINEAR_SOLVER MyLinearSolver;
+//
+//    typedef typename FemLib::FemNodalFunctionScalar<MyDiscreteSystem>::type MyNodalFunctionScalar;
     typedef typename FemLib::FemNodalFunctionVector<MyDiscreteSystem>::type MyNodalFunctionVector;
-    typedef typename NumLib::TXVectorFunctionAsColumnData<MyNodalFunctionVector> NodalPointScalarWrapper;
+//    typedef typename NumLib::TXVectorFunctionAsColumnData<MyNodalFunctionVector> NodalPointScalarWrapper;
 
     ///
-    FunctionDisplacement()
-    : ProcessLib::AbstractTimeIndependentProcess("XFEM_DEFORMATION", 0, 1),
-      _feObjects(0), _displacement(0)//, _strain(0), _stress(0)
+    FunctionXFEM_EXAMPLE_CRACK1()
+    : ProcessLib::AbstractTimeIndependentProcess("XFEM_DEFORMATION", 0, 0),
+      _feObjects(0) //, _displacement(0)//, _strain(0), _stress(0)
     {
         // set default parameter name
-        this->setOutputParameterName(Displacement, "Displacement");
+        //this->setOutputParameterName(Displacement, "Displacement");
     };
 
     ///
-    virtual ~FunctionDisplacement()
+    virtual ~FunctionXFEM_EXAMPLE_CRACK1()
     {
-        BaseLib::releaseObject(_feObjects, _displacement /*, _strain, _stress*/);
+        BaseLib::releaseObject(_feObjects /*, _displacement , _strain, _stress*/);
     }
 
     /// initialize this process
@@ -80,13 +82,13 @@ protected:
 private:
     //void calculateStressStrain();
 
-    DISALLOW_COPY_AND_ASSIGN(FunctionDisplacement);
+    DISALLOW_COPY_AND_ASSIGN(FunctionXFEM_EXAMPLE_CRACK1);
 
 private:
     FemLib::LagrangianFeObjectContainer* _feObjects;
     NumLib::DiscreteDataConvergenceCheck _checker;
     MyNodalFunctionVector* _displacement;
-    std::vector<NodalPointScalarWrapper*> _vec_u_components;
+//    std::vector<NodalPointScalarWrapper*> _vec_u_components;
 //    FemLib::FEMIntegrationPointFunctionVector* _strain;
 //    FemLib::FEMIntegrationPointFunctionVector* _stress;
     MeshLib::IMesh* _msh;
@@ -96,5 +98,5 @@ private:
 
 }
 
-#include "Displacement.hpp"
+//#include "Displacement.hpp"
 
