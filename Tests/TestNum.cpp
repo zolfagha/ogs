4ac2d26f2ef95a45601c0104df18da50b4739a97
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-11-15 18:02:49 +0100

    add a test case of TXFunctionAnalytical

--- Tests/TestNum.cpp
+++ Tests/TestNum.cpp
@@ -15,77 +15,82 @@
 #include <vector>
 
 #include "BaseLib/CodingTools.h"
-#include "BaseLib/BidirectionalMap.h"
 
-#include "MathLib/LinAlg/Dense/Matrix.h"
-#include "MathLib/LinAlg/LinearEquation/DenseLinearEquation.h"
-#include "MathLib/LinAlg/LinearEquation/SparseLinearEquation.h"
-#ifdef USE_LIS
-#include "MathLib/LinAlg/LinearEquation/LisLinearEquation.h"
-#endif
+#include "GeoLib/Point.h"
 
-#include "GeoLib/Rectangle.h"
-
-#include "MeshLib/Tools/MeshGenerator.h"
-#include "MeshLib/Core/IMesh.h"
-
-
-#include "DiscreteLib/Serial/DiscreteSystem.h"
-#include "DiscreteLib/Core/IDiscreteLinearEquation.h"
-#include "DiscreteLib/Utils/DofEquationIdTable.h"
-#include "DiscreteLib/Core/IDiscreteLinearEquationAssembler.h"
-#include "DiscreteLib/Core/IElemenetWiseLinearEquationLocalAssembler.h"
-#include "DiscreteLib/Utils/SparsityBuilderFromNodeConnectivity.h"
-#ifdef USE_MPI
-#include "DiscreteLib/ogs5/par_ddc_group.h"
-#endif
-#include "NumLib/Function/Function.h"
 #include "NumLib/TimeStepping/TimeSteppingController.h"
 #include "NumLib/TimeStepping/TimeStepFunction.h"
+
+#include "NumLib/Function/TXFunctionAnalytical.h"
 
 //#include "NumLib/Output/Output.h"
 
 #include "TestUtil.h"
 
-using namespace GeoLib;
-using namespace MathLib;
-using namespace MeshLib;
-using namespace NumLib;
-using namespace DiscreteLib;
-
-
-
 TEST(Num, TimeStepping1)
 {
     // define problems and solution strategy
-    class TestProblem : public ITransientSystem
+    class TestProblem : public NumLib::ITransientSystem
     {
     private:
         double _t0, _tn, _dt, _v;
     public:
         TestProblem(double t0, double tn, double dt) : _t0(t0), _tn(tn), _dt(dt), _v(.0) {};
         virtual ~TestProblem() {};
-        int solveTimeStep(const TimeStep &/*time*/) { _v+=1.0; return 0; }
-        double suggestNext(const TimeStep &time_current)
+        int solveTimeStep(const NumLib::TimeStep &/*time*/) { _v+=1.0; return 0; }
+        double suggestNext(const NumLib::TimeStep &time_current)
         {
             if (time_current.getTime() < _t0) return _t0;
             else return (time_current.getTime()+_dt <= _tn) ? time_current.getTime()+_dt : time_current.getTime();
         }
-        bool isAwake(const TimeStep &time)  { return (time.getTime()>=_t0 && time.getTime()<=_tn); }
-        void accept(const TimeStep &/*time*/) {};
+        bool isAwake(const NumLib::TimeStep &time)  { return (time.getTime()>=_t0 && time.getTime()<=_tn); }
+        void accept(const NumLib::TimeStep &/*time*/) {};
         double getValue() {return _v;};
     };
     TestProblem problem(.0, 100., 10.);
 
     // pass it to discretization systems
-    TimeSteppingController timeStepping;
+    NumLib::TimeSteppingController timeStepping;
     timeStepping.setTransientSystem(problem);
     // start time stepping
     timeStepping.setBeginning(.0);
     size_t n_steps = timeStepping.solve(100.);
     //check
-    ASSERT_EQ(10, n_steps);
+    ASSERT_EQ(10U, n_steps);
     ASSERT_EQ(10., problem.getValue());
 }
 
+TEST(Num, TXFunctionAnalytical)
+{
+    std::string str_f1 = "100 + 1*x + 2*y + 3*z";
+    NumLib::TXFunctionAnalytical tx_f1(str_f1);
 
+    double v;
+    {
+        GeoLib::Point pt1(0., 0., 0.);
+        tx_f1.eval(NumLib::TXPosition(pt1.getData()), v);
+        ASSERT_NEAR(100., v, 1e-8);
+    }
+
+    {
+        GeoLib::Point pt1(1., 1., 1.);
+        tx_f1.eval(NumLib::TXPosition(pt1.getData()), v);
+        ASSERT_NEAR(106., v, 1e-3);
+    }
+
+    std::string str_f2 = "-100+-1*x+-2*y+-3*z";
+    NumLib::TXFunctionAnalytical tx_f2(str_f2);
+
+    {
+        GeoLib::Point pt1(0., 0., 0.);
+        tx_f2.eval(NumLib::TXPosition(pt1.getData()), v);
+        ASSERT_NEAR(-100., v, 1e-3);
+    }
+
+    {
+        GeoLib::Point pt1(1., 1., 1.);
+        tx_f2.eval(NumLib::TXPosition(pt1.getData()), v);
+        ASSERT_NEAR(-106., v, 1e-3);
+    }
+
+}
