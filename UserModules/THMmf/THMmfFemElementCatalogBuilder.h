8f6ccbb6a986c7c2cfee18af383af5721e05e541
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-11-28 19:32:21 +0100

    in progress to create mapping table between mesh elements and Fe types

--- UserModules/THMmf/THMmfFemElementCatalogBuilder.h
+++ UserModules/THMmf/THMmfFemElementCatalogBuilder.h
@@ -13,6 +13,7 @@
 #pragma once
 
 #include "FemLib/Tools/FemElementCatalog.h"
+#include "FemLib/Tools/MeshElementGroupAndShapeToFemElementType.h"
 #include "FemLib/Core/Element/C0IsoparametricElements.h"
 #include "C0InterfaceElements.h"
 #include "THMmfFiniteElementType.h"
@@ -23,7 +24,7 @@ namespace THMmf
 class THMmfFemElementCatalogBuilder
 {
 public:
-    static void construct(FemLib::FemElementCatalog &feCatalog)
+    static void construct(const std::vector<size_t> &vec_m_group_id, const std::vector<size_t> &vec_ie_group_id, FemLib::FemElementCatalog &feCatalog, FemLib::MeshElementGroupAndShapeToFemElementType &shape2fetype)
     {
         feCatalog.registerFeType<FemLib::LINE2>(THMmfFiniteElementType::LINE2);
         feCatalog.registerFeType<FemLib::LINE3>(THMmfFiniteElementType::LINE3);
@@ -36,6 +37,21 @@ public:
         feCatalog.registerFeType<FemLib::TET10>(THMmfFiniteElementType::TET10);
         feCatalog.registerFeType<IE_TRI3>(THMmfFiniteElementType::IE_TRI3);
         feCatalog.registerFeType<IE_QUAD4>(THMmfFiniteElementType::IE_QUAD4);
+
+        for (size_t i=0; i<vec_m_group_id.size(); ++i) {
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::LINE, 1, THMmfFiniteElementType::LINE2);
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::LINE, 2, THMmfFiniteElementType::LINE3);
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::QUAD, 1, THMmfFiniteElementType::QUAD4);
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::QUAD, 2, THMmfFiniteElementType::QUAD9);
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::TRIANGLE, 1, THMmfFiniteElementType::TRI3);
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::TRIANGLE, 2, THMmfFiniteElementType::TRI6);
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::TETRAHEDRON, 1, THMmfFiniteElementType::TET4);
+            shape2fetype.addFeType(vec_m_group_id[i], MeshLib::ElementShape::TETRAHEDRON, 2, THMmfFiniteElementType::TET10);
+        }
+        for (size_t i=0; i<vec_ie_group_id.size(); ++i) {
+            shape2fetype.addFeType(vec_ie_group_id[i], MeshLib::ElementShape::TRIANGLE, 1, THMmfFiniteElementType::IE_TRI3);
+            shape2fetype.addFeType(vec_ie_group_id[i], MeshLib::ElementShape::QUAD, 1, THMmfFiniteElementType::IE_QUAD4);
+        }
     }
 };
 
