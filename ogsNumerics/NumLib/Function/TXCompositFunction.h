6c236b40a0d38ebaf85c00aa38af0a6d19b501fd
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-11-30 19:09:20 +0100

    extract class Multiplication

--- ogsNumerics/NumLib/Function/TXCompositFunction.h
+++ ogsNumerics/NumLib/Function/TXCompositFunction.h
@@ -19,28 +19,37 @@ namespace NumLib
 {
 
 /**
- *
- */
-template <typename Tval>
-class Multiplication
-{
-public:
-    void doit(const Tval &v1, const Tval &v2, Tval &val)
-    {
-        val = v1 * v2;
-    }
-};
-
-/**
+ * \brief Temporal-spatial function made of two functions
  *
  */
 template <class T1, class T2, template <typename> class T_OPERATOR>
 class TXCompositFunction : public ITXFunction
 {
 public:
-    TXCompositFunction(T1* f1, T2* f2) : _f1(f1), _f2(f2) {};
-    virtual ~TXCompositFunction() {};
+    /**
+     *
+     * @param f1
+     * @param f2
+     */
+    TXCompositFunction(T1* f1, T2* f2) : _f1(f1), _f2(f2)
+    {
+        isConst(_f1->isConst() && _f2->isConst());
+        isTemporallyConst(_f1->isTemporallyConst() && _f2->isTemporallyConst());
+        isSpatiallyConst(_f1->isSpatiallyConst() && _f2->isSpatiallyConst());
+    };
 
+    /**
+     *
+     */
+    virtual ~TXCompositFunction()
+    {
+    };
+
+    /**
+     *
+     * @param x
+     * @param val
+     */
     virtual void eval(const TXPosition &x, DataType &val) const
     {
         DataType v1, v2;
@@ -50,6 +59,11 @@ public:
         op.doit(v1, v2, val);
     }
 
+    /**
+     *
+     * @param x
+     * @param val
+     */
     virtual void eval(const double* x, double &val) const
     {
         double v1, v2;
@@ -59,6 +73,10 @@ public:
         op.doit(v1, v2, val);
     }
 
+    /**
+     *
+     * @return
+     */
     virtual TXCompositFunction* clone() const
     {
         return new TXCompositFunction<T1, T2, T_OPERATOR>(_f1, _f2);
