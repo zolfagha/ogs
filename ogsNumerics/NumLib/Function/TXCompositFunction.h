9fe8888ce4dceaa9d9daaf5a21dac09c7ca6237d
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-12-06 16:38:35 +0100

    store copy of given functions in TXCompositeFunction

--- ogsNumerics/NumLib/Function/TXCompositFunction.h
+++ ogsNumerics/NumLib/Function/TXCompositFunction.h
@@ -31,7 +31,7 @@ public:
      * @param f1
      * @param f2
      */
-    TXCompositFunction(T1* f1, T2* f2) : _f1(f1), _f2(f2)
+    TXCompositFunction(T1* f1, T2* f2) : _f1(f1->clone()), _f2(f2->clone())
     {
         isConst(_f1->isConst() && _f2->isConst());
         isTemporallyConst(_f1->isTemporallyConst() && _f2->isTemporallyConst());
@@ -39,10 +39,23 @@ public:
     };
 
     /**
+     * Copy constructor
+     * @param src
+     */
+    TXCompositFunction(const TXCompositFunction &src)
+    : _f1(src._f1->clone()), _f2(src._f2->clone())
+    {
+        isConst(_f1->isConst() && _f2->isConst());
+        isTemporallyConst(_f1->isTemporallyConst() && _f2->isTemporallyConst());
+        isSpatiallyConst(_f1->isSpatiallyConst() && _f2->isSpatiallyConst());
+    }
+
+    /**
      *
      */
     virtual ~TXCompositFunction()
     {
+        BaseLib::releaseObject(_f1, _f2);
     };
 
     /**
@@ -50,7 +63,7 @@ public:
      * @param x
      * @param val
      */
-    virtual void eval(const TXPosition &x, DataType &val) const
+    virtual void eval(const TXPosition x, DataType &val) const OGS_DECL_OVERRIDE
     {
         DataType v1, v2;
         _f1->eval(x, v1);
@@ -62,9 +75,23 @@ public:
     /**
      *
      * @param x
+     * @param v
+     */
+    virtual void eval(const TXPosition x, double &val) const OGS_DECL_OVERRIDE
+    {
+        double v1, v2;
+        _f1->eval(x, v1);
+        _f2->eval(x, v2);
+        T_OPERATOR<double> op;
+        op.doit(v1, v2, val);
+    }
+
+    /**
+     *
+     * @param x
      * @param val
      */
-    virtual void eval(const double* x, double &val) const
+    virtual void eval(const double* x, double &val) const OGS_DECL_OVERRIDE
     {
         double v1, v2;
         _f1->eval(x, v1);
@@ -77,9 +104,9 @@ public:
      *
      * @return
      */
-    virtual TXCompositFunction* clone() const
+    virtual TXCompositFunction* clone() const OGS_DECL_OVERRIDE
     {
-        return new TXCompositFunction<T1, T2, T_OPERATOR>(_f1, _f2);
+        return new TXCompositFunction<T1, T2, T_OPERATOR>(*this);
     }
 
 private:
