c85e29cd221674be753a40163b0358023d9f646f
Haibing Shao <haibing.shao@ufz.de>
2013-03-19 15:41:08 +0100

    add the class for equilibrium reaction, and 3 variants of eq reaction.

---
+++ ogs6THMC/ChemLib/chemReactionEqMob.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.com)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.com/LICENSE.txt
+ *
+ *
+ * \file chemReactionEqMob.h
+ *
+ * Created on 2013-03-19 by Haibing Shao
+ */
+#include "chemReactionEq.h"
+#include "BaseLib/OrderedMap.h" 
+
+#ifndef CHEM_REACTION_EQ_MOB_H
+#define CHEM_REACTION_EQ_MOB_H
+
+namespace ogsChem
+{
+
+class chemReactionEqMob : public chemReactionEq
+{
+public: 
+	/**
+      * constructor and destructor
+      */
+    chemReactionEqMob(void) : chemReactionEq()
+    {
+        _eqReactType = ogsChem::MOB_EQ_REACT;
+    };
+	~chemReactionEqMob(void);
+
+    /**
+      * the realization of eval function
+      * the returned value is the residual of the reaction. 
+      * if equilibrium is reached, then redisual is zero. 
+      */
+    double eval(ogsChem::LocalVector & vec_log_Conc)
+    {
+        // TODO
+    };
+};
+
+}  // end of namespace
+
+#endif
---
+++ ogs6THMC/ChemLib/chemReactionEqSorp.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.com)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.com/LICENSE.txt
+ *
+ *
+ * \file chemReactionEqSorp.h
+ *
+ * Created on 2013-03-19 by Haibing Shao
+ */
+#include "chemReactionEq.h"
+#include "BaseLib/OrderedMap.h" 
+
+#ifndef CHEM_REACTION_EQ_SORP_H
+#define CHEM_REACTION_EQ_SORP_H
+
+namespace ogsChem
+{
+
+class chemReactionEqSorp : public chemReactionEq
+{
+public: 
+	/**
+      * constructor and destructor
+      */
+    chemReactionEqSorp(void) : chemReactionEq()
+    {
+        _eqReactType = ogsChem::SORP_EQ_REACT;
+    };
+	~chemReactionEqSorp(void);
+
+    /**
+      * the realization of eval function
+      * the returned value is the residual of the reaction. 
+      * if equilibrium is reached, then redisual is zero. 
+      */
+    double eval(ogsChem::LocalVector & vec_log_Conc)
+    {
+        // TODO
+    };
+
+};
+
+}  // end of namespace
+
+#endif
