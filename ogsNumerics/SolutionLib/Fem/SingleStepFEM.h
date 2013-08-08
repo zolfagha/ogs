5219b85e8c9a88f48efd92ccef1f5ca07c5b44f4
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2013-08-08 18:04:57 +0200

    save log in siglestepFEM

--- ogsNumerics/SolutionLib/Fem/SingleStepFEM.h
+++ ogsNumerics/SolutionLib/Fem/SingleStepFEM.h
@@ -138,6 +138,7 @@ private:
     SolutionVector *_x_n1_0;
     SolutionVector *_x_n1;
     SolutionVector *_x_st;
+    BaseLib::Options _opLog;
 };
 
 template <
@@ -206,6 +207,10 @@ SingleStepFEM<T_USER_FEM_PROBLEM,T_LINEAR_SOLVER,T_NL_SOLVER_FACTORY>::SingleSte
 
     // setup nonlinear solver
     _f_nonlinear = new NonlinearSolverType(dis, _f_linear, _f_r, _f_dx, nl_solver_factory);
+    _f_nonlinear->setLog(_opLog);
+
+    // set inital log
+    this->getTimeStepFunction()->updateLog(_opLog);
 };
 
 template <
@@ -275,6 +280,9 @@ int SingleStepFEM<T_USER_FEM_PROBLEM,T_LINEAR_SOLVER,T_NL_SOLVER_FACTORY>::solve
         //SolutionVector* vec_var = _problem->getVariable(i)->getIC()->getNodalValues();
         DiscreteLib::setLocalVector(_dofManager, i, msh_id, *_x_n1, *_vec_u_n1[i]->getDiscreteData());
     }
+
+    // update log
+    this->getTimeStepFunction()->updateLog(_opLog);
 
     return 0;
 }
