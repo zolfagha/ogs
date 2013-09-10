8075a76ca2e3b3e882e0f39db2b11db6c752c77b
Haibing Shao <haibing.shao@ufz.de>
2013-09-10 14:36:36 +0200

    Unity activity coefficient model. All activity coefficients are ones.

---
+++ ogs6THMC/ChemLib/chemActivityModelUnity.h
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.com)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.com/LICENSE.txt
+ *
+ *
+ * \file chemActivityModelAqUnity.h
+ *
+ * Created on 2013-09-10 by Haibing Shao
+ */
+
+#ifndef CHEM_ACTIVITY_MODEL_AQ_UNITY_H
+#define CHEM_ACTIVITY_MODEL_AQ_UNITY_H
+
+#include "chemActivityModelAbstract.h"
+
+namespace ogsChem
+{
+
+/**
+ * \brief Common interface of Activity Model
+ */
+class chemActivityModelAqDBH : chemActivityModelAbstract
+{
+public:
+    // destructor
+    chemActivityModelAqDBH() 
+        : _activity_model( ogsChem::ACT_MOD_UNITY )
+    {
+    };
+
+    /**
+     * calculate the activity based on molarity
+     * @param log_molarity
+	 * @param log_activity
+     */
+    void calc_activity(MathLib::LocalVector & log_molarity, 
+                       MathLib::LocalVector & log_activity, 
+                       const double Tc = 25.0)
+    {
+        log_activity = log_molarity; 
+    }
+
+};
+
+} // end of namespace
+
+#endif
