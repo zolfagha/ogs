e02aeabef2fdce46f4d5a22c12887cff96896568
Haibing Shao <haibing.shao@gmail.com>
2013-11-03 20:34:29 +0100

    same convergence criteria for local problem.

--- UserModules/FemReactGIAReduct/SingleStepGIAReduction.h
+++ UserModules/FemReactGIAReduct/SingleStepGIAReduction.h
@@ -447,7 +447,7 @@ int SingleStepGIAReduction<T_USER_FUNCTION_DATA, T_USER_FEM_PROBLEM, T_USER_LINE
 	}
 
 	//solve local problem
-	_function_data->calc_nodal_local_problem(t_n1.getTimeStepSize(), 1.0E-8, 1.0E-12, 50);  //temp disabled
+	_function_data->calc_nodal_local_problem(t_n1.getTimeStepSize(), 1.0E-11, 1.0E-14, 50);  //temp disabled
 
 	// calculate the reaction rates on each node
 	// _function_data->update_node_GIA_reaction_rates();
