a785bc177bd9f95d994099a5f2029a760be89b8a
zolfagha <reza.zolfaghari@ufz.de>
2013-12-13 13:10:16 +0100

    fixing the line search algorithm

--- UserModules/FemReactGIAReduct/LocalProblem.cpp
+++ UserModules/FemReactGIAReduct/LocalProblem.cpp
@@ -218,7 +218,8 @@ void LocalProblem::solve_LocalProblem_Newton_LineSearch(std::size_t & node_idx,
             // updating dx
             dx = dx * alpha;
             // increment of unknowns
-            this->increment_unknown( x, dx, x_new ); 
+            //this->increment_unknown( x, dx, x_new );
+            x_new = x_new + dx;  //RZ: essentail for convergency
             // now updating the saturation index and minerals
             if(_n_xi_Min != 0)
 				this->update_minerals_conc_AI( x_new, vec_AI );
